2fb33dda0430d6e9a3d2c44ace1fa4298d36488d
mob41 <mob41@users.noreply.github.com>
2017-01-08 16:22:26 +0800

    Change version to 0.0.3 in Osu.java

--- src/main/java/com/github/mob41/osumer/io/Osu.java
+++ src/main/java/com/github/mob41/osumer/io/Osu.java
@@ -29,7 +29,7 @@ public class Osu {
 	
 //TODO: Hard-code version?
 	
-	public static final String OSUMER_VERSION = "0.0.1";
+	public static final String OSUMER_VERSION = "0.0.3";
 	
 	public static final String OSUMER_BRANCH = "snapshot";
 	
