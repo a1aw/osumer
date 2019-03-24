ea223dcb140324c37c20fb690684d12815d17bed
mob41 <mob41@users.noreply.github.com>
2019-03-24 11:16:37 +0800

    Remove files and add IUI RMI file

---
+++ osumer-lib/src/main/java/com/github/mob41/osumer/daemon/IUI.java
@@ -0,0 +1,12 @@
+package com.github.mob41.osumer.daemon;
+
+import java.io.IOException;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+import com.github.mob41.osumer.queue.Queue;
+import com.github.mob41.osumer.queue.QueueManager;
+
+public interface IUI extends Remote{
+    
+}
