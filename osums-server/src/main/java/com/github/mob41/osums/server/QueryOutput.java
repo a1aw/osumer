40b47f877a7a1fa73105aa028e085237aa8a9ebf
mob41 <mob41@users.noreply.github.com>
2019-08-19 15:25:45 +0800

    Add files

---
+++ osums-server/src/main/java/com/github/mob41/osums/server/QueryOutput.java
@@ -0,0 +1,17 @@
+package com.github.mob41.osums.server;
+
+import com.github.mob41.osums.search.ResultBeatmap;
+
+public class QueryOutput {
+
+	public int result;
+	
+	public String msg;
+	
+	public int currentPage;
+	
+	public int totalPages;
+	
+	public ResultBeatmap[] output;
+	
+}
